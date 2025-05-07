891949b6149591a17e19e118fe5d1f4f32d2b42e
NicholasCorniaOrpheus <nicholas.cornia@orpheusinstituut.be>
2025-05-07 13:23:31 +0200

    2025-05-07

---
+++ orcid/main.py
@@ -0,0 +1,23 @@
+"""Main script for ORCID"""
+
+import sys
+
+sys.path.append("./modules")
+
+from modules.api import *
+from modules.utilities import *
+
+print("Importing credentials...")
+credentials = json2dict("../credentials/credentials.json")["orcid"]
+
+# Test
+
+nicholas_orcid = "0000-0003-1114-6710"
+
+# get access token
+access_token = orcid_public_token(credentials)
+nicholas_works = get_works(nicholas_orcid, access_token)
+
+print("Getting works...")
+output_works_filename = "./data/works/test.json"
+dict2json(nicholas_works, output_works_filename)
